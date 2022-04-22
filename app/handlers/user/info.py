43bac87f397e3a09e54292a906f75f5c986bbc2a
misha <misha@kurapov.ru>
2022-04-22 21:42:22 +0300

    add parents information

--- app/handlers/user/info.py
+++ app/handlers/user/info.py
@@ -46,4 +46,30 @@ async def info_handler(message: Message, f: FMT):
     )
 
 
+@dp.message(commands="parents")
+async def parents_handler(message: Message, f: FMT):
+    user = await f.db.get_user(message.from_user.id)
+    cookies = pickle.loads(user.cookies)
+    session = Session(cookies=cookies)
+    user_information = await (
+        await session.get(
+            "https://lyceum.yandex.ru/api/profile",
+            params={
+                "withCoursesSummary": "false",
+                "withExpelled": "false",
+                "withChildren": "false",
+                "withParents": "true",
+            },
+        )
+    ).json()
+    text = ''
+    if user_information['profile']['parents']:
+        text += "ℹ️ <b>Информация о родителях:</b> \n\n"
+        for parent in user_information['profile']['parents']:
+            text += f"▫️ <b>ФИО:</b> {parent['lastName']} {parent['firstName']} {parent['middleName']} \n"
+            text += f"       <b>Телефон:</b> {parent['phone']} \n" if parent['phone'] else ""
+            text += f"       <b>E-mail:</b> {parent['email']} \n" if parent['email'] else ""
+            text += "\n"
 
+    text += f"🎚 <b>Родителей добавлено: {user_information['profile']['invite']['usesCount']}/{user_information['profile']['invite']['usesLimit']}</b>\n\n"
+    await message.answer(text, reply_markup=get_invite_keyboard(user_information['profile']['invite']['url']))
