cbae18f59593e4d989d2b91e768945e91a71b6ff
misha <misha@kurapov.ru>
2022-04-25 17:30:27 +0300

    add solution api request

--- app/utils/api.py
+++ app/utils/api.py
@@ -4,11 +4,11 @@ from aiohttp import ClientSession
 
 
 async def get_user_information(
-    with_courses_summary: bool,
-    with_expelled: bool,
-    with_children: bool,
-    with_parents: bool,
-    cookies: bytes,
+        with_courses_summary: bool,
+        with_expelled: bool,
+        with_children: bool,
+        with_parents: bool,
+        cookies: bytes,
 ):
     cookies = pickle.loads(cookies)
     session = ClientSession(cookies=cookies)
@@ -21,5 +21,15 @@ async def get_user_information(
                 "withChildren": str(with_children).lower(),
                 "withParents": str(with_parents).lower(),
             },
+        )
+    ).json()
+
+
+async def get_solution_information(solution_id, cookies):
+    cookies = pickle.loads(cookies)
+    session = ClientSession(cookies=cookies)
+    return await (
+        await session.get(
+            f"https://lyceum.yandex.ru/api/student/solutions/{solution_id}"
         )
     ).json()
