6906bbf8bc1300ba3e236c8f9b6e80a4866eb9c7
misha <misha@kurapov.ru>
2022-05-01 00:35:06 +0300

    api update

--- app/utils/api.py
+++ app/utils/api.py
@@ -33,3 +33,59 @@ async def get_solution_information(solution_id, cookies):
             f"https://lyceum.yandex.ru/api/student/solutions/{solution_id}"
         )
     ).json()
+
+
+async def get_lessons_information(course_id, group_id, cookies):
+    cookies = pickle.loads(cookies)
+    session = ClientSession(cookies=cookies)
+    return await (
+        await session.get(
+            "https://lyceum.yandex.ru/api/student/lessons/",
+            params={"courseId": course_id, "groupId": group_id},
+        )
+    ).json()
+
+
+async def get_lesson_info(lesson_id, course_id, group_id, cookies):
+    cookies = pickle.loads(cookies)
+    session = ClientSession(cookies=cookies)
+    return await (
+        await session.get(
+            f"https://lyceum.yandex.ru/api/student/lessons/{lesson_id}",
+            params={"courseId": course_id, "groupId": group_id},
+        )
+    ).json()
+
+
+async def get_lesson_tasks(lesson_id, course_id, group_id, cookies):
+    cookies = pickle.loads(cookies)
+    session = ClientSession(cookies=cookies)
+    return await (
+        await session.get(
+            "https://lyceum.yandex.ru/api/student/lessonTasks",
+            params={"courseId": course_id, "groupId": group_id,
+                    "lessonId": lesson_id},
+        )
+    ).json()
+
+
+async def get_task_info(task_id, group_id, cookies):
+    cookies = pickle.loads(cookies)
+    session = ClientSession(cookies=cookies)
+    return await (
+        await session.get(
+            f"https://lyceum.yandex.ru/api/student/tasks/{task_id}",
+            params={"groupId": group_id},
+        )
+    ).json()
+
+
+async def get_notifications(cookies, is_read=False):
+    cookies = pickle.loads(cookies)
+    session = ClientSession(cookies=cookies)
+    return await (
+        await session.get(
+            "https://lyceum.yandex.ru/api/notifications",
+            params={"isRead": str(is_read)},
+        )
+    ).json()
